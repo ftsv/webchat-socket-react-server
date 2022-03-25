736f0b858fa114f35b8ce5108483b21c670b7b46
ftsv <fetisov_fd@icloud.com>
2022-03-25 16:42:17 +0700

    add timezones route

--- router.js
+++ router.js
@@ -3,6 +3,55 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('server is up and running');
+});
+
+router.get('/timezones', (req, res) => {
+    res.send(JSON.parse([
+  {
+    "timezone": "+2",
+    "name": "Калининград"
+  },
+  {
+    "timezone": "+3",
+    "name": "Москва"
+  },
+  {
+    "timezone": "+4",
+    "name": "Самара"
+  },
+  {
+    "timezone": "+5",
+    "name": "Екатеринбург"
+  },
+  {
+    "timezone": "+6",
+    "name": "Омск"
+  },
+  {
+    "timezone": "+7",
+    "name": "Красноярск"
+  },
+  {
+    "timezone": "+8",
+    "name": "Иркутск"
+  },
+  {
+    "timezone": "+9",
+    "name": "Якутск"
+  },
+  {
+    "timezone": "+10",
+    "name": "Владивосток"
+  },
+  {
+    "timezone": "+11",
+    "name": "Магадан"
+  },
+  {
+    "timezone": "+12",
+    "name": "Петропавловск-Камчатский"
+  }
+]))
 })
 
 module.exports = router; 
